75fd9d623d32bcf37e792839fb0cb912fab8d7df
Tushar <tushar.jadhav.work@gmail.com>
2024-04-29 10:00:22 +0530

    Addition Function Added

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,13 +1,29 @@
-//import java.util.*;
-// interface MathOper{
-//      int calculate(int x,int y);
-//}
+import java.util.*;
+ interface MathOper{
+      int calculate(int x,int y);
+}
 
 public class Calculator {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Calculator program");
 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number : ");
+        int x = sc.nextInt();
+        System.out.println("Enter the second number : ");
+        int y = sc.nextInt();
+
+
+
+        while(true){
+            System.out.println("***---Welcome to Calculator---***");
+            System.out.println("\n 1.Addition \n 2.Substraction \n 3.Multiplication \n 4.Division");
+            switch (sc.nextInt()){
+                case 1 :
+                    MathOper add = (a,b)-> x + y ;
+                    System.out.println("Addition : "+add.calculate(x,y));
+            }
+        }
 
     }
 }
