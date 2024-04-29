7f29f3507aa1a8a999908109f48ed952f1b00ceb
Tushar <tushar.jadhav.work@gmail.com>
2024-04-29 09:53:50 +0530

    Add file

---
+++ src/Calculator.java
@@ -0,0 +1,13 @@
+//import java.util.*;
+// interface MathOper{
+//      int calculate(int x,int y);
+//}
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Calculator program");
+
+
+    }
+}
